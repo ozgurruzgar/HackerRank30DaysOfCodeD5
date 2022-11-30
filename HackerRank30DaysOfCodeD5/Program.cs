5df057ed74a6fb29011de71b69ba9e94a8396f1a
Özgür <zgr@DESKTOP-1NLBACI>
2022-12-01 01:34:53 +0300

    Add project files.

---
+++ HackerRank30DaysOfCodeD5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HackerRank30DaysOfCodeD5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int result;
+            if (n >= 2 && n <= 20)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    result = n * i;
+                    Console.WriteLine(n + " x " + i + " = " + result);
+                }
+            }
+        }
+    }
+}
